51eb73e7470004006f1ca7ab69225bf76f1d0eaa
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2018-09-13 23:12:41 +0300

    update env variables to use names declared in https://www.postgresql.org/docs/9.1/static/libpq-envars.html

--- db.js
+++ db.js
@@ -1,14 +1,12 @@
 require('dotenv').config({path: '../.env'});
 
 const connection = {
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DB,
-  port: process.env.POSTGRES_PORT,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
 };
-
-console.log(process.env.POSTGRES_USER,)
 
 const pool = {
   min: 2,
