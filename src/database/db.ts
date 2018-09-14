486f21215b47673d97f4b7dc5b89202c3acf78ed
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2018-09-14 17:44:11 +0300

    add query function
    function takes objection querybuilder as argument and updates audit and error logs when needed

---
+++ src/database/db.ts
@@ -0,0 +1,14 @@
+import { Model, QueryBuilder } from "objection";
+import { writeAuditLogEntry, writeErrorLogEntry } from "./logger";
+
+export const query = async (queryBuilder: QueryBuilder<Model>) =>  {
+  try {
+    if (!queryBuilder.isFind()) { // should append to audit log if inserting / updating values to the db
+      await writeAuditLogEntry(queryBuilder.toSql());
+    }
+    await queryBuilder; // if audit written succesfully, attempt to query db
+  } catch (e) {
+    // TODO: remove audit log entry if inserted
+    await writeErrorLogEntry(e);
+  }
+};
