7cb4129642883424b3cc0a28241e141835aa0c59
Juho Jokela <juho.jokela@atalent.fi>
2018-10-09 14:18:06 +0300

    Add comment how query should work

--- src/organisations/router.ts
+++ src/organisations/router.ts
@@ -1,7 +1,19 @@
+// import subWeeks from "date-fns/sub_weeks";
 import bodyParser from "koa-bodyparser";
 import Router from "koa-router";
 
+// import { AuditModel } from "../audit/audit";
+
 export default new Router({prefix: "/organisations"})
+  // .get("/:organisation/events", (ctx) => {
+  //   const events = AuditModel.find({
+  //     organisation: ctx.params.organisation,
+  //     timestamp: {
+  //       $gte: subWeeks(Date.now(), 2),
+  //     },
+  //   });
+  //   ctx.body = { events };
+  // })
   .get("/:organisation", (ctx) => {
     ctx.body = {
       id: ctx.params.organisation,
