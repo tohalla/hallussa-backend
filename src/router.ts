98c69f9d551c9047fe431adea0c956daa354b30a
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2018-10-15 15:53:02 +0300

    update to singular folder names

--- src/router.ts
+++ src/router.ts
@@ -2,11 +2,12 @@ import Router from "koa-router";
 
 import accounts from "./account/router";
 import auth from "./auth/router";
-import organisations from "./organisations/router";
+import organisationsRouter from "./organisation/router";
 
-const router = new Router({prefix: "/api/v1"});
+const router = new Router({ prefix: "/api/v1" });
 
-[accounts, auth, organisations]
-  .forEach((r) => router.use(r.routes(), r.allowedMethods()));
+[accounts, auth, organisationsRouter].forEach((r) =>
+  router.use(r.routes(), r.allowedMethods())
+);
 
 export default router;
