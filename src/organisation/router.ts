f30795fbcf55959733cf7e2dea82fd0d6c9bc6cc
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2018-10-15 16:47:57 +0300

    update routes for fetching organisations

--- src/organisation/router.ts
+++ src/organisation/router.ts
@@ -1,11 +1,17 @@
 import bodyParser from "koa-bodyparser";
 import Router from "koa-router";
+import Organisation from "./Organisation";
 
-export default new Router({prefix: "/organisations"})
-  .get("/:organisation", (ctx) => {
-    ctx.body = {
-      id: ctx.params.organisation,
-    };
+export default new Router({ prefix: "/organisations" })
+  .get("/", async (ctx) => {
+    // TODO: Should organisations be public? if so, limit public data
+    ctx.body = await Organisation.query().select();
+  })
+  .get("/:organisation", async (ctx) => {
+    ctx.body = await Organisation.query()
+      .select()
+      .where("id", "=", ctx.params.organisation)
+      .first();
   })
   .post("/", bodyParser(), () => {
     // create organisation
