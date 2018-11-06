1641c665ceb101e16455e68f1023def4de5ec635
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2018-11-06 12:56:16 +0200

    add model for maintenanceEvent

---
+++ src/maintenance/MaintenanceEvent.ts
@@ -0,0 +1,41 @@
+import { Model } from "objection";
+import Appliance from "../appliance/Appliance";
+
+export default class MaintenanceEvent extends Model {
+  public static tableName = "maintenance_event";
+
+  public static relationMappings = {
+    appliance: {
+      join: {
+        from: "maintenance_event.appliance",
+        to: "appliance.id",
+      },
+      modelClass: Appliance,
+      relation: Model.BelongsToOneRelation,
+    },
+  };
+
+  public static jsonSchema = {
+    type: "object",
+
+    properties: {
+      id: {type: "integer"},
+
+      appliance: {type: "integer"},
+    },
+    required: ["appliance"],
+  };
+
+  public id?: number;
+  public updatedAt?: string;
+  public createdAt?: string;
+  public appliance?: number;
+
+  public async $beforeUpdate() {
+    delete this.id; // should not update id field
+    delete this.createdAt; // should not update createdAt field
+    delete this.appliance; // should not update appliance field
+
+    this.updatedAt = new Date().toISOString();
+  }
+}
