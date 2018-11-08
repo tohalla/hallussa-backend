e8fc71780f97f4137c3d616bab09a33b4bac0149
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2018-11-08 15:01:28 +0200

    update moves qrpage generation to separate file

--- src/appliance/router.ts
+++ src/appliance/router.ts
@@ -1,8 +1,7 @@
 import bodyParser from "koa-bodyparser";
 import Router from "koa-router";
-import QRCode from "qrcode";
-import { path, reduce } from "ramda";
 
+import { getQRPage } from "../util/qr";
 import Appliance from "./Appliance";
 
 export default new Router({ prefix: "/appliances" })
@@ -33,36 +32,8 @@ export default new Router({ prefix: "/appliances" })
       return ctx.throw(400, "Define requested appliance");
     }
 
-    // fetch specified appliances (only if listed under organisation)
-    const appliances = await Appliance
-      .query()
-      .select("hash")
-      .whereIn("id", applianceIDs)
-      .andWhere("organisation", "=", organisation);
-
-    if (appliances.length === 0) {
-      return ctx.throw(404);
-    }
-    const qrCodes = await Promise.all(
-      appliances.map(async (appliance) =>
-        QRCode.toString(
-          `http://${ctx.request.host}/api/v1/maintenance/${appliance.hash}`,
-          {errorCorrectionLevel: "medium", type: "svg", scale: 2}
-        ),
-        appliances
-      )
-    );
-
-    const qrElements = reduce(
-      (prev, curr) => prev + `<div style=\"width: 250px; height: auto;\">${
-        curr
-      }</div>`,
-      "",
-      qrCodes
-    );
-
     ctx.body = `<!doctype html><html><head></head><body>${
-      qrElements
+      await getQRPage(applianceIDs, organisation, ctx.request.host)
     }</body></html>`;
     ctx.type = "application/html";
   })
