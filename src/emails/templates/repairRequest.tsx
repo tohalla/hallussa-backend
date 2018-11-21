20bfa6df885f606c9d09f0c75470b2f027c28274
Hai Phan <hatazhix@gmail.com>
2018-11-21 16:58:13 +0200

    Changed email style and texts

--- src/emails/templates/repairRequest.tsx
+++ src/emails/templates/repairRequest.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { renderToStaticMarkup } from "react-dom/server";
 import Footer from "./components/Footer";
 import Signature from "./components/Signature";
+import MaintenanceEvent from "./../../maintenance/MaintenanceEvent";
 
 interface Appliance {
   name: string;
@@ -10,25 +11,29 @@ interface Appliance {
   description: string;
 }
 
-export default (contents: { appliance: Appliance, request: string }) => {
+export default (contents: {
+  appliance: Appliance;
+  maintenanceEvent: MaintenanceEvent;
+  request: string;
+}) => {
   const {
-    appliance: {
-      name, model, manufacturer, description,
-    },
-    request,
+    appliance: { name, model, manufacturer, description },
+    maintenanceEvent: { description: maintenanceDescription },
+    request
   } = contents;
   return renderToStaticMarkup(
     <div>
       <h2>An appliance has been reported malfunctioning.</h2>
-      <p>Details:</p>
+      <p>Details of the appliance:</p>
       <ul>
-        <li>{ name }</li>
-        <li>{ model }</li>
-        <li>{ manufacturer }</li>
-        <li>{ description }</li>
+        <li>Name: {name}</li>
+        <li>Model: {model}</li>
+        <li>Manufacturer: {manufacturer}</li>
+        <li>Description: {description}</li>
+        <li>Short description of the problem: {maintenanceDescription}</li>
       </ul>
-      <p>Repair request:</p>
-      <p>{ request }</p>
+      <p>Accept repair request:</p>
+      <p>{request}</p>
       <Signature />
       <Footer />
     </div>
