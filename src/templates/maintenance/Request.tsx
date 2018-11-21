534c4f4c9966800012d7bc961939a47cd39e2f7b
Juho Jokela <juho.jokela@atalent.fi>
2018-11-21 14:28:53 +0200

    Add temporary DOM for maintenance request
    Use this to test that we can load styles properly

---
+++ src/templates/maintenance/Request.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { renderToStaticMarkup } from "react-dom/server";
+
+export default (hash: string) => renderToStaticMarkup(
+  <body>
+    <div>
+      
+    </div>
+  </body>
+);
