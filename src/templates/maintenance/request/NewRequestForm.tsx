587087bda84a8e89c96fcf5782b002980064122a
Juho Jokela <juho.jokela@atalent.fi>
2018-11-22 13:47:10 +0200

    Some changes to DOM of submitting new maintenance request

--- src/templates/maintenance/request/NewRequestForm.tsx
+++ src/templates/maintenance/request/NewRequestForm.tsx
@@ -2,11 +2,25 @@ import React from "react";
 
 import button from "../../../emotion-styles/src/button";
 import { actionsRow, form, inputRow } from "../../../emotion-styles/src/form";
+import { label } from "../../../emotion-styles/src/inline";
+import input from "../../../emotion-styles/src/input";
 
 export default ({hash}: { hash: string }) => (
   <form method="POST" className={form} action={`./${hash}`}>
-    <input type="text" name="description"></input>
+    <label className={label} htmlFor="description">
+      Description
+    </label>
+    <div className={inputRow}>
+      <textarea
+        className={input}
+        style={{ height: "200px" }}
+        name="description"
+        id="description"
+        placeholder="Short description of the problem"
+      />
+    </div>
     <div className={actionsRow}>
+      <span></span>
       <button className={button} type="submit">
         Send maintenance report
       </button>
--- src/templates/maintenance/request/Request.tsx
+++ src/templates/maintenance/request/Request.tsx
@@ -2,16 +2,51 @@ import { renderStylesToString } from "emotion-server";
 import React from "react";
 import { renderToStaticMarkup } from "react-dom/server";
 
-// import {} from "../../../emotion-styles/src/button";
-
+import container, { body } from "../../../emotion-styles/src/container";
+import { logoContainer, uppercaseTitle } from "../../../emotion-styles/src/topbar";
+import Footer from "../../shared/Footer";
+import TopBar from "../../shared/TopBar";
+import ViewContainer from "../../shared/ViewContainer";
 import NewRequestForm from "./NewRequestForm";
 
 export default (hash: string, name: string, organisation: string) => renderStylesToString(renderToStaticMarkup(
-  <body>
-    <h1>{name}</h1>
-    <h3>{organisation}</h3>
-    <div>
-      <NewRequestForm hash={hash} />
-    </div>
-  </body>
+  <html>
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </head>
+    <body className={body}>
+      <ViewContainer>
+        <TopBar>
+          <div style={{ display: "flex" }}>
+            <div className={logoContainer} style={{ position: "absolute" }}>
+              logo
+            </div>
+            <div className={uppercaseTitle} style={{ flexGrow: 1 }}>
+              Maintenance Report
+            </div>
+          </div>
+        </TopBar>
+        <div className={container}>
+          <div style={{ width: "90vw", margin: "0 auto" }}>
+            <h1>{name}</h1>
+            <h3>{organisation}</h3>
+            <p>Hey!</p>
+            <p>
+              You have scanned a maintenance card.
+              You can report a malfunctioning appliance on this form.
+              We’ll make sure that the appliance’s organisation is informed as soon as you send this report.
+              Thanks for the effort!
+            </p>
+            <p>
+              Leave your email address and you will be informed when the appliance is functioning again.
+            </p>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <NewRequestForm hash={hash} />
+          </div>
+        </div>
+        <Footer />
+      </ViewContainer>
+    </body>
+  </html>
 ));
