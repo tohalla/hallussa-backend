a7bcf51dbd2a5bb9ff4b5d8aacd6acf63851fed7
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2019-04-17 11:08:44 +0300

    add translations for appliance

--- seeds/01_translations.ts
+++ seeds/01_translations.ts
@@ -5,16 +5,24 @@ import seedFile from "knex-seed-file";
 
 export const seed = async (knex: Knex) => {
   await knex("language").del();
-  return seedFile(
+
+  const seedFileOptions = {
+    columnSeparator: ";",
+  };
+
+  await seedFile(
     knex,
     path.resolve(__dirname, "translation", "language.csv"),
     "language",
-    {
-      columnSeparator: ";",
-      handleInsert: (inserts: any, tableName: string) => knex.raw(
-        knex(tableName).insert(inserts).toString().replace("?", "\\?") +
-        " ON CONFLICT DO NOTHING"
-      ),
-    }
+    seedFileOptions
   );
+
+  return Promise.all([
+    ["translation", "en.csv"],
+  ].map(([table, fileName]) => seedFile(
+    knex,
+    path.resolve(__dirname, "translation", fileName),
+    table,
+    seedFileOptions
+  )));
 };
