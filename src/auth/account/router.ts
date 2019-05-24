0ca155a5d28cefcf47935a086232192e5e6a80bb
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2019-05-24 19:49:24 +0300

    add route for changing password

--- src/auth/account/router.ts
+++ src/auth/account/router.ts
@@ -1,6 +1,7 @@
+import bcrypt from "bcryptjs";
 import bodyParser from "koa-bodyparser";
 import Router from "koa-router";
-import { path } from "ramda";
+import { dissoc, path, prop } from "ramda";
 
 import { secureRoute } from "../jwt";
 import Account from "./Account";
@@ -40,8 +41,26 @@ export default new Router({prefix: "/accounts"})
       throw e;
     }
   })
-  .patch("/", secureRoute, bodyParser(), async (ctx) => {
+  .use(secureRoute)
+  .patch("/:account", bodyParser(), async (ctx) => {
     ctx.body = await Account.query().patch(
-      ctx.request.body || {}
+      dissoc("password", ctx.request.body || {}) // use separate route for updating password
     ).returning("*").first();
+  })
+  .put("/:account/password", bodyParser(), async (ctx) => {
+    const {account} = ctx.params;
+    const {password, currentPassword, retypePassword} = ctx.request.body as {[key: string]: string};
+
+    if (typeof password !== "string" || typeof currentPassword !== "string" || password !== retypePassword) {
+      return ctx.throw(401, "Invalid request body");
+    }
+
+    const hashed = prop("password", await Account.query().select("password").where("id", account).first());
+
+    if (typeof hashed === "string" && (await bcrypt.compare(currentPassword, hashed))) {
+      await Account.query().patch({password}).where("id", account);
+      ctx.status = 200;
+    } else {
+      ctx.throw(401, "Incorrect current password");
+    }
   });
